856a81f521e765e4ef698b0730f1ca342d70e3db
Aldy Victor Rianes <aldyvictor1046@gmail.com>
2021-12-05 16:50:47 +0700

    Integration Vue Done

--- resources/js/Pages/Chat/chatRoomSelection.vue
+++ resources/js/Pages/Chat/chatRoomSelection.vue
@@ -1,7 +1,35 @@
 <template>
-    <div>Selection View</div>
+    <div class="grid grid-cols-2">
+        <div class="font-bold text-xl">{{ selected.name }} Chat</div>
+        <div>
+            <select v-model="selected" @change="push" class="float-right">
+                <option
+                    :value="room"
+                    v-for="(room, index) in rooms"
+                    :key="index"
+                >
+                    {{ room.name }}
+                </option>
+            </select>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {};
+export default {
+    props: ["rooms", "currentRoom"],
+    data: function () {
+        return {
+            selected: "",
+        };
+    },
+    methods: {
+        push() {
+            this.$emit("roomchanged", this.selected);
+        },
+    },
+    created() {
+        this.selected = this.currentRoom;
+    },
+};
 </script>
--- resources/js/Pages/Chat/container.vue
+++ resources/js/Pages/Chat/container.vue
@@ -2,15 +2,23 @@
     <app-layout title="Chat">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Chat
+                <chat-room-selection
+                    v-if="currentRoom.id"
+                    :rooms="chatRooms"
+                    :currentRoom="currentRoom"
+                    v-on:roomchanged="setRoom"
+                />
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <message-container />
-                    <input-message :room="currentRoom" />
+                    <message-container :messages="messages" />
+                    <input-message
+                        :room="currentRoom"
+                        v-on:messagesent="getMessages()"
+                    />
                 </div>
             </div>
         </div>
@@ -22,16 +30,18 @@ import { defineComponent } from "vue";
 import AppLayout from "@/Layouts/AppLayout.vue";
 import MessageContainer from "./messageContainer.vue";
 import InputMessage from "./inputMessage.vue";
+import ChatRoomSelection from "./chatRoomSelection.vue";
 
 export default defineComponent({
     components: {
         AppLayout,
         MessageContainer,
         InputMessage,
+        ChatRoomSelection,
     },
     data: function () {
         return {
-            chatRoom: [],
+            chatRooms: [],
             currentRoom: [],
             messages: [],
         };
@@ -41,7 +51,7 @@ export default defineComponent({
             axios
                 .get("/chat/rooms")
                 .then((response) => {
-                    this.chatRoom = response.data;
+                    this.chatRooms = response.data;
                     this.setRoom(response.data[0]);
                 })
                 .catch((error) => {
